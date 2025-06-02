8343032291234231f078e0898434da84c3a87d52
Alexander Zarubin <whitesashka@gmail.com>
2025-06-02 15:16:54 +0500

    IPcalc in progress

---
+++ IPcalc/main.cpp
@@ -0,0 +1,56 @@
+#include<Windows.h>
+#include"resource.h"
+#include<CommCTRL.h>
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG_MAIN), NULL, (DLGPROC)DlgProc, 0);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HWND hPrefix = GetDlgItem(hwnd, IDC_SPIN_PREFIX);
+		SendMessage(hPrefix, UDM_SETRANGE,0,MAKELPARAM( 32, 1));
+		SetFocus(GetDlgItem(hwnd,IDC_IPADDRESS));
+	}
+
+		break;
+	case WM_COMMAND:
+	{
+		HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
+		HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+		DWORD dwIPaddress = 0;
+		DWORD dwIPmask = 0;
+		switch (LOWORD(wParam))
+		{
+		case IDC_IPADDRESS:
+		{
+			SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
+			if (FIRST_IPADDRESS(dwIPaddress) < 128)SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFF000000);
+			else if (FIRST_IPADDRESS(dwIPaddress) < 192)SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFFFF0000);
+			else if (FIRST_IPADDRESS(dwIPaddress) < 224)SendMessage(hIPmask, IPM_SETADDRESS, 0, 0xFFFFFF00);
+
+
+		}
+
+		case IDOK:
+			break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
+		}
+
+	}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
